5e2fe0cc1e4185309b62e478eefaa6c5fc662567
prabh <prabh@LAPTOP-SHSMVAML.fios-router.home>
2024-12-27 13:16:11 -0500

    chapter 2: Added gitignore file.

--- obj/Debug/net9.0/api2.AssemblyInfo.cs
+++ obj/Debug/net9.0/api2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7e5826dfd4ae8d5df0e92f1dc25522fd033450b")]
 [assembly: System.Reflection.AssemblyProductAttribute("api2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
